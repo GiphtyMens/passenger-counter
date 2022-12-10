d4396a1c9593423799f5ae726f175ac81feb3d8b
GiphtyMens <giftymens111@gmail.com>
2022-12-10 21:53:31 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,201 @@
+// let myAge = 27;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
+
+// let bonusPoints = 50;
+// bonusPoints = bonusPoints + 50;
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints - 75;
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints + 45;
+// console.log(bonusPoints);
+
+// function increment() {
+//     console.log("The button was clicked")
+// }
+
+// function countdown() {
+//   console.log(5);
+//   console.log(4);
+//   console.log(3);
+//   console.log(2);
+//   console.log(1);
+// }
+
+// countdown();
+
+// function number() {
+//     console.log(42)
+// }
+
+// number();
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// function sumTotal() {
+//     let total = lap1 + lap2 + lap3
+//     console.log(total);
+// }
+
+// sumTotal();
+
+// let lapsCompleted = 0
+
+// function incrementLap() {
+//     lapsCompleted = lapsCompleted + 1
+//     console.log(lapsCompleted);
+// }
+
+// incrementLap();
+// incrementLap();
+// incrementLap();
+
+// console.log(lapsCompleted);
+
+// document.getElementById("count-el").innerText = 0
+
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+console.log(saveEl)
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+    
+
+
+
+
+
+// increment();
+
+// let countEl = document.getElementById("count-el");
+// console.log(countEl);
+
+// let count = 0
+// function increment() {
+//     count = count + 1
+//     countEl.innerText = count
+// }
+
+// increment();
+
+// function save() {
+//     console.log(count)
+// }
+
+// save();
+
+// let username = "per";
+
+// let message = "You have three new notifications";
+
+// let messageToUser = "We have logged"
+
+// console.log(messageToUser + " " + message + " " + username)
+
+
+// let nome = "Giphty"
+// let greeting = "Hi, my name is "
+// let myGreeting = greeting + nome
+
+// console.log(myGreeting)
+
+// function greeting() {
+//     console.log(42)
+// }
+
+
+let 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
